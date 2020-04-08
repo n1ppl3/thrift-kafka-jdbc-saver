1051cabbac9abe0e8e95fe0e73e4c4f561251980
n1ppl3 <n1ppl3@gmail.com>
2020-04-08 16:21:57 +0300

    NonNul fix

--- src/main/java/ru/n1ppl3/thrift/kafka/jdbc/saver/kafka/ConsumerStartedAppListener.java
+++ src/main/java/ru/n1ppl3/thrift/kafka/jdbc/saver/kafka/ConsumerStartedAppListener.java
@@ -1,10 +1,10 @@
 package ru.n1ppl3.thrift.kafka.jdbc.saver.kafka;
 
 import lombok.Getter;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.context.ApplicationListener;
 import org.springframework.kafka.event.ConsumerStartedEvent;
 import org.springframework.kafka.event.KafkaEvent;
+import org.springframework.lang.NonNull;
 
 import java.util.concurrent.CountDownLatch;
 
@@ -15,7 +15,7 @@ public class ConsumerStartedAppListener implements ApplicationListener<KafkaEven
     private final CountDownLatch countDownLatch = new CountDownLatch(2); // ConcurrentKafkaListenerContainerFactory concurrency
 
     @Override
-    public void onApplicationEvent(@NotNull KafkaEvent event) {
+    public void onApplicationEvent(@NonNull KafkaEvent event) {
         System.err.println(" <<< - - - - - - - - " + event + " - - - - - - - - >>> ");
         if (event instanceof ConsumerStartedEvent) {
             countDownLatch.countDown();
