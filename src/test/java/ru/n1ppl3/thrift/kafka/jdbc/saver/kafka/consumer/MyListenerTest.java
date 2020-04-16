7c58e3555d779569ee063729ba75a9a3f2128a14
n1ppl3 <n1ppl3@gmail.com>
2020-04-16 14:33:42 +0300

    ConfigFileApplicationContextInitialize example

--- src/test/java/ru/n1ppl3/thrift/kafka/jdbc/saver/kafka/consumer/MyListenerTest.java
+++ src/test/java/ru/n1ppl3/thrift/kafka/jdbc/saver/kafka/consumer/MyListenerTest.java
@@ -4,11 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -26,10 +28,10 @@ import static ru.n1ppl3.thrift.kafka.jdbc.saver.SharedTestData.r3;
 
 @Slf4j
 @EmbeddedKafka
+@ActiveProfiles({"test"})
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {SpringKafkaConfiguration.class, MyListener.class})
-@TestPropertySource(locations = {"classpath:application.properties", "classpath:application-test.properties"},
-    properties = {"spring.kafka.consumer.group-id=MyListenerTest-group", "my.topic.name=${MyListenerTest.topic}"}) // чтобы не было пересечений с другим тестом
+@ContextConfiguration(initializers = {ConfigFileApplicationContextInitializer.class}, classes = {SpringKafkaConfiguration.class, MyListener.class})
+@TestPropertySource(properties = {"spring.kafka.consumer.group-id=MyListenerTest-group", "my.topic.name=${MyListenerTest.topic}"}) // чтобы не было пересечений с другим тестом
 class MyListenerTest {
 
     private static final String TOPIC_NAME = MyListenerTest.class.getSimpleName() + "Topic-" + Instant.now().toEpochMilli();
